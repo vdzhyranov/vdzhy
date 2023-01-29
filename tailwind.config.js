a98f8535e5300adc03c842b78fb78e1b7f0bb3ed
vDzhyranov <v.dzhyranov@gmail.com>
2023-01-29 22:25:54 +0200

    hide files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
